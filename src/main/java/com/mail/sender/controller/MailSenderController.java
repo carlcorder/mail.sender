c3d8debe5e5a793a30bd1e1a8baa5814e4b3a645
Carl Corder <corder.carl@gmail.com>
2016-07-14 00:05:11 -0500

    adding form validation to the email domain object

--- src/main/java/com/mail/sender/controller/MailSenderController.java
+++ src/main/java/com/mail/sender/controller/MailSenderController.java
@@ -3,9 +3,14 @@ package com.mail.sender.controller;
 import com.mail.sender.domain.Email;
 import com.mail.sender.service.MailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 public class MailSenderController {
@@ -14,11 +19,13 @@ public class MailSenderController {
     private MailSenderService mailSenderService;
 
     @RequestMapping(value = "/send", method = RequestMethod.POST)
-    public ResponseEntity<Email> sendMessage(@RequestBody Email email) {
-        if(mailSenderService.send(email) != null) {
-            return new ResponseEntity<Email>(email, HttpStatus.OK);
+    public String sendMessage(@RequestBody @Valid Email email, BindingResult bindingResult) {
+        if(!bindingResult.hasErrors() && mailSenderService.send(email) != null) {
+            return "success";
         }
-        return new ResponseEntity<Email>(email, HttpStatus.INTERNAL_SERVER_ERROR);
+        return bindingResult.getAllErrors()
+                .stream().map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
     }
 
 }
--- src/main/java/com/mail/sender/domain/Email.java
+++ src/main/java/com/mail/sender/domain/Email.java
@@ -1,6 +1,8 @@
 package com.mail.sender.domain;
 
-import lombok.NonNull;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @lombok.Data
 @lombok.AllArgsConstructor
@@ -8,13 +10,14 @@ import lombok.NonNull;
 @lombok.Builder
 public class Email {
 
-    @NonNull
+    @NotNull
+    @Pattern(regexp = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}", message = "not a valid email address")
     private String to;
 
-    @NonNull
     private String from;
 
-    @NonNull
+    @NotNull
+    @Size(min = 1, message = "subject is empty")
     private String subject;
 
     private String body;
